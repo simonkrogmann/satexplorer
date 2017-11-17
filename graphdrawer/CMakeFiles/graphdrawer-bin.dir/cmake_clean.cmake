12a468fbbd3cdca03ec213f7cbfb53d4a5de90f1
Juliane Waack <jkwaack@gmail.com>
2017-11-17 01:00:49 -0800

    combined graphdrawer and satviewer projects in cmake

---
+++ graphdrawer/CMakeFiles/graphdrawer-bin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphdrawer-bin.dir/main.cpp.o"
+  "CMakeFiles/graphdrawer-bin.dir/cfg.cpp.o"
+  "CMakeFiles/graphdrawer-bin.dir/ogdfWrapper.cpp.o"
+  "../graphdrawer-bin.pdb"
+  "../graphdrawer-bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphdrawer-bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ satviewer/CMakeFiles/satviewer-bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/satviewer-bin.dir/main.cpp.o"
+  "../satviewer-bin.pdb"
+  "../satviewer-bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/satviewer-bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
