36e17b693faaad759fed100cd05f1384abb36baf
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2018-02-16 10:49:57 +0100

    play around with clustering

--- clustering.py
+++ clustering.py
@@ -5,6 +5,8 @@ import numpy as np
 import itertools
 from random import shuffle
 from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
 from sklearn import metrics
 import sys
 np.random.seed(1)
@@ -24,9 +26,16 @@ with open(sys.argv[1], "r") as file:
 numpy_features = np.array(feature_list)
 
 kmeans = KMeans(n_clusters=14, n_init=20, max_iter=300, n_jobs=-2)
+#aggc = AgglomerativeClustering(14)
+#spectral = SpectralClustering()
 
 kmeans.fit(numpy_features)
+#aggc.fit(numpy_features)
+#spectral.fit(numpy_features)
+
+labels = kmeans.labels_
+#labels = spectral.labels_
 
 with open(sys.argv[2], "w") as file:
     for i in range(len(node_ids)):
-        file.write(node_ids[i] + " " + str(kmeans.labels_[i]) + "\n")
+        file.write(node_ids[i] + " " + str(labels[i]) + "\n")
